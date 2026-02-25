b2575d8daf3ec12dfa566570fdf6c0e249e8581c
staslolpol <krutalevichlev@gmail.com>
2026-02-25 18:46:20 +0300

    update 5

---
+++ Task03/main.cpp
@@ -0,0 +1,36 @@
+#include "iostream"
+using namespace std;
+
+bool search_binary(int array[10], int length, int value) {
+	int first = 0;
+	int last = length - 1;
+
+	while (first <= last) {
+		int middle = (first + last) / 2;
+
+		if (array[middle] == value) {
+			return true;
+		}
+		else if (array[middle] < value) {
+			first = middle + 1;
+		}
+		else {
+			last = middle - 1;
+		}
+	}
+
+	return false;
+}
+
+int main() {
+	int array[20]{1,2,3,4,5,6,7,8,9,10};
+	int size = {10}; // int size = 10;
+
+	int value = 7;
+
+
+	cout << "Does value exist ? - ";
+	cout << (search_binary(array, size, value) ? "Yes" : "No");
+
+	return 0;
+}
